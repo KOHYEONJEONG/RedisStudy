1c5dc962f9d513e2d4c37e3adcbf9e6062e2d742
DESKTOP-275CC7F\EM_NB163 <hyeonjeong.ko@easymedia.net>
2024-12-16 17:13:25 +0900

    redis 의존성 및 설정하여 postman으로 실행(로그를 통해 캐시 생성 및 조회 확인)

--- src/main/java/org/example/redisspring/BoardController.java
+++ src/main/java/org/example/redisspring/BoardController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("boards")
+@RequestMapping("/boards")
 public class BoardController {
 
     private BoardService boardService;
@@ -17,8 +17,11 @@ public class BoardController {
         this.boardService = boardService;
     }
 
-    @GetMapping("/")
+    @GetMapping()
     public List<Board> getBoards(@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int size) {
-        return boardService.getBoards(page, size);
+        return boardService.getBoards(page, size); // 1, 10
+
+        //캐시 생성 로그  : Creating cache entry for key 'boards:page:1:size:10' in cache(s) [getBoards]
+        //새로고침 시 (캐시로 조회) : Cache entry for key 'boards:page:1:size:10' found in cache(s) [getBoards]
     }
 }
--- src/main/java/org/example/redisspring/BoardService.java
+++ src/main/java/org/example/redisspring/BoardService.java
@@ -1,5 +1,6 @@
 package org.example.redisspring;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -16,6 +17,10 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
+    // @Cacheable : Cache Aside 전력으로 캐싱이 적용됨.
+        //    요청이 들어오면 레디스를 확인한 후에 데이터가 있다면 레디스의 데이터를 조회해서 바로 응답한다.
+        //    만약 데이터가 없다면 메서드 내부의 로직을 실행시킨 뒤에 return 값으로 응답한다. 그리고 그 return 값을 레디스에 저장
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size", cacheManager = "boardCacheManager")
     public List<Board> getBoards(int page, int size){
 
         Pageable pageable = PageRequest.of(page - 1, size);
